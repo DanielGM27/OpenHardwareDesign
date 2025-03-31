c75b8134b937be57283f12fbfe3fe7140a884c90
DanielGM27 <danielgilmorales99@gmail.com>
2025-03-31 08:25:30 +0200

    .gitignore añadido

--- obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_threads.cpp \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilatedos.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_threads.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_config.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_types.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_funcs.h
--- obj_dir/verilated_timing.d
+++
@@ -1,8 +0,0 @@
-verilated_timing.o: \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_timing.cpp \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_timing.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilatedos.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_config.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_types.h \
- /home/daniel/miniconda3/envs/labs-dpaa/share/verilator/include/verilated_funcs.h
